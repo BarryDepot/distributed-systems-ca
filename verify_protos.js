e4599a5559e5bbd1579a781eb23af19dc4e58dd9
BarryDepot <barriomalley@gmail.com>
2026-05-08 01:01:15 +0100

    set up grpc deps and verify protos load

---
+++ verify_protos.js
@@ -0,0 +1,27 @@
+// making sure all 3 proto files load without errors
+
+const grpc = require('@grpc/grpc-js');
+const protoLoader = require('@grpc/proto-loader');
+const path = require('path');
+
+const protos = [
+  'safety_monitor.proto',
+  'resource_access.proto',
+  'incident_reporting.proto',
+];
+
+console.log('Verifying proto files...');
+
+protos.forEach((file) => {
+  const def = protoLoader.loadSync(path.join(__dirname, 'protos', file), {
+    keepCase: true,
+    longs: String,
+    enums: String,
+    defaults: true,
+    oneofs: true,
+  });
+  grpc.loadPackageDefinition(def);
+  console.log(`  ${file} - OK`);
+});
+
+console.log('All protos loaded successfully.');
